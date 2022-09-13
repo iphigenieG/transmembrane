a4ddfb5132332b9fff1f06e47f347f578c692b36
Iphigenie_Gonnet <iphigenie.g@gmail.com>
2022-09-13 13:15:59 +0200

    create class Sphere

--- sphere.py
+++ sphere.py
@@ -1,40 +1,40 @@
 import math
-import matplotlib.pyplot as plt
+import points
+#import matplotlib.pyplot as plt
 
+class Sphere ():
+    def __init__(self,samples=100):
+        self.point_list = self.__fibonacci_sphere(samples)
 
-def fibonacci_sphere(samples=1000):
+    def __fibonacci_sphere(self,samples):
+        point_list = []
+        phi = math.pi * (3. - math.sqrt(5.))  # golden angle in radians
 
-    xs = []
-    ys = []
-    zs = []
-    phi = math.pi * (3. - math.sqrt(5.))  # golden angle in radians
+        for i in range(samples):
 
-    for i in range(samples):
+            y = 1 - (i / float(samples - 1)) # y goes from 1 to 0
 
-        y = 1 - (i / float(samples - 1)) # y goes from 1 to 0
+            radius = math.sqrt(1 - y * y)  # radius at y
 
-        radius = math.sqrt(1 - y * y)  # radius at y
+            theta = phi * i  # golden angle increment
 
-        theta = phi * i  # golden angle increment
+            x = math.cos(theta) * radius
+            z = math.sin(theta) * radius
 
-        x = math.cos(theta) * radius
-        z = math.sin(theta) * radius
+            point = points.Coord(x,y,z)
+            point_list.append(point)
 
-        xs.append(x)
-        ys.append(y)
-        zs.append(z)
+        return point_list
 
-    return xs,ys,zs
+#     def show_scatter(self):
+#         fig = plt.figure()
+#         ax = fig.add_subplot(projection='3d')
 
-xs,ys,zs = fibonacci_sphere()
+#         for point in self.point_list :
+#             ax.scatter3D(*(point.get()), color = "blue")
+#         ax.scatter(0,-1,0, color="red")
+#         plt.show()
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-
-xs.append(0)
-ys.append(-1)
-zs.append(0)
-
-ax.scatter(xs, ys, zs)
-
-plt.show()
+# if __name__ == "__main__":
+#     sphere = Sphere(100)
+#     sphere.show_scatter()
